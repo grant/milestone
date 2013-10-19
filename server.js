d793e572036d1f6adebb4066dab6669591d1f6ae
asimov4 <asimov4@gmail.com>
2013-10-19 13:32:29 -0400

    experiment 2 to repair openshift

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ var SampleApp = function() {
     self.initializeServer = function() {
         self.createRoutes();
         self.app = express.createServer();
-        // self.app.use(express.static(__dirname + '/static'));
+        self.app.use(express.static(__dirname + '/static'));
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
