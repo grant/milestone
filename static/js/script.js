f5e2bce84571c88ab0fcd3d30ae288eaa424fb5d
Grant Timmerman <granttimmerman@gmail.com>
2013-11-03 00:07:52 -0700

    Added templates to person view

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,42 @@
 $(function() {
+
+	// Get results
+	var data = [
+		{
+			name: "Grant Timmerman",
+			matchPercent: 84,
+			match: {
+				percent: 84,
+				color: "#48c9b0"
+			},
+			profilePic: "https://scontent-a.xx.fbcdn.net/hphotos-ash3/1075634_531762900206769_1498370310_n.jpg",
+			skillProgress: 45,
+			skillMarginLeft: 30,
+			skills: [
+				{name: "Java", have: true},
+				{name: "Scala", have: true},
+				{name: "HTML", have: false},
+				{name: "CSS", have: true},
+				{name: "C++", have: false},
+				{name: "C", have: true}
+			],
+			education: {
+				university: {name:"University of Washington",have:false},
+				major: {name:"Math",have:false},
+				degree: {name:"PhD",have:false}
+			},
+			experienceProgress: 25,
+			experienceMarginLeft: 20,
+			experience: [
+				{company:"Google", position:"Senior Software Engineer"},
+				{company:"Yahoo", position:"QA Engineer"},
+				{company:"Zynga", position:"Software Engineer in Test"},
+				{company:"Groupon", position:"Software Engineer Intern"}
+			]
+		}
+	];
+
+	// Setup mouse/touch events
 	var $body = $('body');
 	$body.click(function() {
 		throwOutCard('right');
@@ -18,8 +56,8 @@ $(function() {
 		if (!throwing) {
 			// Add new card underneath
 			var $oldCard = $('.card:last');
-
-
+			var $card = getCard();
+			$('.cards').prepend($card);
 
 			// Throw out card
 			throwing = true;
@@ -33,5 +71,21 @@ $(function() {
 				$(this).remove();
 			});
 		}
-	};
+	}
+
+	var cardIndex = 0;
+	function getCard() {
+		if (cardIndex === data.length) {
+			cardIndex = 0;
+		}
+		var personData = data[cardIndex];
+		++cardIndex;
+		return template(personData);
+	}
+
+	var template;
+	$.get('./static/hbs/personCard.hbs', function(data) {
+		template = Handlebars.compile(data);
+		$('.cards').append(getCard());
+	});
 });
