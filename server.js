f857be6b439cd35cae83779cd30784ce76ee5241
asimov4 <asimov4@gmail.com>
2013-12-01 22:22:18 -0500

    Removed unused code

--- server.js
+++ server.js
@@ -58,65 +58,6 @@ var SampleApp = function () {
         }
     };
 
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function () {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = {
-                indexHTML: ''
-            };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function (key) {
-        return self.zcache[key];
-    };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function (sig) {
-        if (typeof sig === "string") {
-            console.log('%s: Received %s - terminating sample app ...', Date(Date.now()), sig);
-            process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()));
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function () {
-        //  Process on exit and signals.
-        process.on('exit', function () {
-            self.terminator();
-        });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-            'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function (element, index, array) {
-            process.on(element, function () {
-                self.terminator(element);
-            });
-        });
-    };
-
-
     /*  ================================================================  */
     /*  App server functions (main app logic here).					   */
     /*  ================================================================  */
@@ -211,12 +152,6 @@ var SampleApp = function () {
             res.send("");
         };
 
-        // New index
-        self.routes['/'] = function (req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html'));
-        };
-
         self.routes['/api'] = function (req, res) {
             var view = res;
             // Setup
@@ -308,8 +243,6 @@ var SampleApp = function () {
      */
     self.initialize = function () {
         self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
 
         // Create the express server and routes.
         self.initializeServer();
